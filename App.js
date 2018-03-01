176b4bf868b8ef1c04cdc3f9dd7f393c0bc4c255
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-03-01 19:56:49 +0100

    Finished

---
+++ App.js
@@ -0,0 +1,33 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  X-Client-Id: 2951,
+  X-Auth-Token: e929251bf7d4bb3fabc24e95cbf01a81
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(cardObj);
+  	})
+}
+
